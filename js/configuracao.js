9fff3ca9bff1654280aa3f7e85069981f59f3936
Diego Fernandes <diegorlfernandes@hotmail.com>
2017-04-02 10:35:30 -0400

    configuracao

---
+++ js/configuracao.js
@@ -0,0 +1,22 @@
+
+$(function() {
+    (function(configuracao) {
+		configuracao.iniciar = function(){
+			configuracao.ExecutarEventos();
+		}
+		
+		configuracao.ExecutarEventos = function(){
+			$(document).on('pagebeforechange',function(e,data){
+                var toPage = data.toPage[0].id;
+                switch (toPage)	{
+                    case 'pgConfiguracao':
+						$('#pgAltConfiguracaoMesAno').datepicker( "setDate",new Date());
+					break;
+				}
+			});
+		}
+		configuracao.iniciar();
+    })(Database);
+
+});
+
